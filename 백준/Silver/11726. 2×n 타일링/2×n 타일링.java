513b5d404a200b20ab0bf934dbd01767f498543d
gagle1231 <88420412+gagle1231@users.noreply.github.com>
2022-12-18 20:06:08 +0900

    Update 2×n 타일링.java

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,18 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[1001]; // new int[n+1] 로 하면  런타임 에러(ArrayIndexOutOfBounds) 발생 new int[1001]로 바꿔줌 
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
-        }
-        System.out.println(dp[n]);
-    }
+	public static int solution(int a[], int n) {
+		a[0] = a[1] = 1;
+		
+		for(int i=2; i<=n; i++) {
+			a[i] = a[i-1] + a[n-2];
+		}
+		return a[n]%10007;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		int[] a = new int[n+1];
+
+		
+		System.out.println(solution(a, n));
+
+	}
 }
