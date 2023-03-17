4f2cc8a872e552a6dd8139f8836a07d62ac3a522
gagle1231 <88420412+gagle1231@users.noreply.github.com>
2023-03-17 16:36:06 +0900

    [Silver I] Title: 카드 구매하기, Time: 300 ms, Memory: 21212 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n+1];
+		int[] p = new int[n+1];
+		
+		for(int i=1; i<=n; i++) {
+			arr[i] = sc.nextInt();
+			p[i] =arr[i];
+		}
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=i; j++) {
+				p[i] = Math.max(p[i], p[i-j]+arr[j]);
+			}
+		}
+		System.out.println(p[n]);
+	}
+
+}
