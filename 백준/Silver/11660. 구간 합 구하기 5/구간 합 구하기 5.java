11cab695851d7e23d08248ab131150924fb0f36e
gaeunK <88420412+gagle1231@users.noreply.github.com>
2024-06-22 15:16:13 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1748 ms, Memory: 124664 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] sub = new int[n+1][n+1];
+
+        //1행 1열
+        for(int i=1; i<=n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=1; j<=n; j++){
+                sub[i][j] = sub[i-1][j] + sub[i][j-1] + Integer.parseInt(st.nextToken()) - sub[i-1][j-1];
+            }
+        }
+
+        for(int i=1; i<=m; i++){
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken())-1;
+            int y1 = Integer.parseInt(st.nextToken())-1;
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            int as = sub[x2][y2] - (sub[x2][y1]+sub[x1][y2]) + sub[x1][y1];
+            System.out.println(as);
+        }
+    }
+}
