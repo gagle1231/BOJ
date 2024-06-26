28d998202f77922c36069834d30138a4452185ea
gaeunK <88420412+gagle1231@users.noreply.github.com>
2024-06-26 22:00:49 +0900

    [Silver III] Title: 현대모비스 입사 프로젝트, Time: 640 ms, Memory: 57584 KB -BaekjoonHub

--- 백준/Silver/27922. 현대모비스 입사 프로젝트/현대모비스 입사 프로젝트.java
+++ 백준/Silver/27922. 현대모비스 입사 프로젝트/현대모비스 입사 프로젝트.java
@@ -8,17 +8,16 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        int[][] arr  = new int[n][3]; //역량
-        int[][] w = new int[n][3];
+        int[][] w = new int[n][3]; 
         for(int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-            arr[i][2] = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
-            w[i][0] = arr[i][0] + arr[i][1];
-            w[i][1] = arr[i][0] + arr[i][2];
-            w[i][2] = arr[i][1] + arr[i][2];
+            w[i][0] = a + b;
+            w[i][1] = a + c;
+            w[i][2] = b + c;
         }
 
         int answer = 0;
