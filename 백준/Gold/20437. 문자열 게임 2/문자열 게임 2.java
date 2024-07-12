e8d8bd94055e9d789bd1daa09af69b9def02b971
gaeunK <88420412+gagle1231@users.noreply.github.com>
2024-07-12 22:11:40 +0900

    [Gold V] Title: 문자열 게임 2, Time: 412 ms, Memory: 40716 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -4,40 +4,40 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int t = Integer.parseInt(st.nextToken());
-        for(int i=0; i<t; i++){
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
             String str = br.readLine();
-            int w = Integer.parseInt(br.readLine());
-            solution(str, w);
+            int k = Integer.parseInt(br.readLine());
+            solution(str, k);
         }
     }
-    public static void solution(String word, int w){
-        int[] alpha = new int[27];
-        int size = word.length();
-        for(char c: word.toCharArray()){
-            alpha[c - 'a']++;
-        }
 
-        int max = 0;
-        int min = size + 1;
-        for(int i=0; i<size-w+1; i++){
-            char c = word.charAt(i);
-            if(alpha[c-'a'] < w) continue;
-            int cnt=0;
-            for(int j=i; j<size; j++){
-                if (word.charAt(j) == c) cnt++;
-                if(cnt == w){
-                    int len = j - i + 1;
-                    min = min > len ? len : min;
-                    max = max > len ? max : len;
-                    break;
+    public static void solution(String word, int k) {
+        int n = word.length();
+        int minLength = Integer.MAX_VALUE;
+        int maxLength = Integer.MIN_VALUE;
+        boolean found = false;
+
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            LinkedList<Integer> queue = new LinkedList<>();
+            for (int i = 0; i < n; i++) {
+                if (word.charAt(i) == ch) {
+                    queue.add(i);
+                }
+                if (queue.size() == k) {
+                    int length = queue.getLast() - queue.getFirst() + 1;
+                    minLength = Math.min(minLength, length);
+                    maxLength = Math.max(maxLength, length);
+                    queue.poll();
+                    found = true;
                 }
             }
         }
-        if(max == 0) System.out.println(-1);
-        else System.out.println(min+" "+max);
 
+        if (found) {
+            System.out.println(minLength + " " + maxLength);
+        } else {
+            System.out.println(-1);
+        }
     }
 }
