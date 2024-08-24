cceea648d1e1daedaddeba1e24f5ad2851e4e9d9
gaeunK <88420412+gagle1231@users.noreply.github.com>
2024-08-24 18:58:35 +0900

    [Silver IV] Title: 수 찾기, Time: 524 ms, Memory: 47620 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,31 +1,40 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Integer n = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        HashSet<Integer> set = new HashSet<>(n); // 초기 용량을 n으로 설정
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        HashSet<Integer> set = new HashSet<>();
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             set.add(Integer.parseInt(st.nextToken()));
         }
 
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        StringBuilder builder = new StringBuilder();
-        for(int i=0; i<m; i++){
+
+        StringBuilder builder = new StringBuilder(m * 2); // 대략적인 초기 크기 설정
+        for (int i = 0; i < m; i++) {
             int k = Integer.parseInt(st.nextToken());
-            if(set.contains(k)){
+            if (set.contains(k)) {
                 builder.append(1);
-            }else {
+            } else {
                 builder.append(0);
             }
             builder.append("\n");
         }
-        System.out.println(builder);
+
+        bw.write(builder.toString());
+        bw.flush();
+        bw.close();
     }
 }
