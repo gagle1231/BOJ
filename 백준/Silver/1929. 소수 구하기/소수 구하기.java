271b7296630faccf5f71bd08a3d143ca0d1c5b2f
gaeunK <88420412+gagle1231@users.noreply.github.com>
2024-10-11 00:22:11 +0900

    [Silver III] Title: 소수 구하기, Time: 276 ms, Memory: 23348 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,31 +1,29 @@
-import java.util.*;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
 
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
-		int n, m;
+        boolean[] arr = new boolean[n + 1];
+        arr[0] = true;
+        arr[1] = true;
 
-		n=sc.nextInt();
-		m=sc.nextInt();
-		boolean[] arr = new boolean[m+1];
-		
-		arr[1] = true; 
-		
-		for(int i=2; i<=m; i++) {
-			if(arr[i]==true)
-				continue;
-			
-			for(int j=2*i; j<=m; j+=i)
-				arr[j]=true;
-		}
-		
-		for(int i=n; i<=m; i++)
-			if(!arr[i])
-				sb.append(i+"\n");
-		
-		System.out.print(sb);
-		sc.close();
-	}
+        for(int i=2; i<=Math.sqrt(n); i++) {
+            if(arr[i]) continue;
+            for(int j=2; i*j<=n; j++) {
+                arr[i*j] = true;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i=m; i<=n; i++) {
+            if(!arr[i]){
+                sb.append(i).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
 }
